de9dd5a2c946998f44a7472de4001d94d5688ff7
Pronepoet <ochiengphillip84@gmail.com>
2022-10-30 22:40:51 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', '8', 'F', 'Black', 'cruel', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', '8', 'M', 'black', 'kind', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', '8', 'F', 'Black', 'greedy', '1' );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergent Brown', '7', 'F', 'Black', 'cool', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melisa', '12', 'M', 'black', 'smart', '1' );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', '6', 'F', 'brown', 'friendly', '1');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', '8', 'F', 'white', 'tough', '1' );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, '9', 'F', 'black', 'cool', '1' );
